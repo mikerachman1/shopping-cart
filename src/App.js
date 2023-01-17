ee9d9303aef13c02a7a11e33861f9ee692fdd242
Mike Rachman <mikerachman1@gmail.com>
2023-01-17 13:23:26 -0500

    Added About.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
