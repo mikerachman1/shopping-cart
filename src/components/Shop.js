e53119433e8db9d6cb2bd839500cba42ad259d48
Mike Rachman <mikerachman1@gmail.com>
2023-01-18 12:12:27 -0500

    added cart link to sticky shop header

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Item from "./Item";
 import padPrices from "./helpers/padPrices";
+import { Link } from "react-router-dom";
 
 const Shop = (props) => {
   const { products, addToCart, numberItems, total } = props;
@@ -8,7 +9,12 @@ const Shop = (props) => {
   return (
     <div className="shop">
       <div className="sticky-cart">
-        <h3>Items in Cart: {numberItems}</h3>
+        <h3>Items in <Link 
+                      className="shop-cart-link" 
+                      to="/cart"
+                      >
+                      Cart
+                      </Link>: {numberItems}</h3>
         <h3>Total: ${padPrices(total)}</h3>
       </div>
       <h1>Shop</h1>
