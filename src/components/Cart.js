846bbef88eedd7638b016418e8f49ec690a36f27
Mike Rachman <mikerachman1@gmail.com>
2023-01-19 15:46:42 -0500

    cart styled

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -13,28 +13,32 @@ const Cart = (props) => {
   
   return (
     <div className="cart">
-      <h1>Cart</h1>
-      <h3>Items in Cart: {totalItems}</h3>
-      <div className="cart-contents">
-        <ul>
-          {cartItems.map((ittItem, i) => (
-            <li key={ittItem.id}>
-              <h4>{ittItem.item.title}</h4>
-              <p>Quantity : {quantityItems[i].number}
-                <button onClick={() => incrementItem(ittItem.id, i)}>+</button>
-                <button onClick={() => decrementItem(ittItem.id, i)}>-</button>
-              </p>
-              <p>Price : {padPrices(ittItem.item.price)}</p>
-              <p>Sub-Total : {padPrices(subtotalItems[i].number)}</p>
-              <button onClick={() => deleteFromCart(ittItem.id, i)}>Delete From Cart</button>
-            </li>
-          ))}
-        </ul>
+      <div className="cart-container">
+        <h1>Cart</h1>
+        <h3>Items in Cart: {totalItems}</h3>
+        <div className="cart-contents">
+          <ul>
+            {cartItems.map((ittItem, i) => (
+              <li key={ittItem.id}>
+                <h4>{ittItem.item.title}</h4>
+                <p>Quantity : {quantityItems[i].number}
+                  <div>
+                    <button onClick={() => incrementItem(ittItem.id, i)}>More</button>
+                    <button onClick={() => decrementItem(ittItem.id, i)}>Less</button>
+                  </div>
+                </p>
+                <p>Price : {padPrices(ittItem.item.price)}</p>
+                <p>Sub-Total : {padPrices(subtotalItems[i].number)}</p>
+                <button onClick={() => deleteFromCart(ittItem.id, i)}>Delete From Cart</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <h3>Order Total: ${padPrices(totalPrice)}</h3>
+        {totalItems > 0 &&
+          <button>Complete Order</button>
+        }
       </div>
-      <h3>Order Total: ${padPrices(totalPrice)}</h3>
-      {totalItems > 0 && 
-        <button>Complete Order</button>
-      }
     </div>
   );
 };
